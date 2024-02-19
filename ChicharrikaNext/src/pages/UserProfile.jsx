e9bc041a73e965e1f4feb6978bb5722377f371b7
Abelardo Acosta Cracco <102688833+Moriarty369@users.noreply.github.com>
2024-02-19 13:06:57 +0100

    fixing user profile

--- ChicharrikaNext/src/pages/UserProfile.jsx
+++ ChicharrikaNext/src/pages/UserProfile.jsx
@@ -1,39 +1,14 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import { useParams, useLocation } from "react-router-dom";
+
 import "../styles/UserProfile.css";
 
-const UserProfile = ({ id }) => {
-  // Estado para almacenar la información del usuario
-  const [userData, setUserData] = useState(null);
+const UserProfile = () => {
+  const { userId } = useParams();
+  // console.log("id es: ", userId);
+  const location = useLocation();
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get("http://localhost:3001/api/register");
-        console.log(response);
-    
-        const contentType = response.headers["content-type"] || response.headers["Content-Type"];
-    
-        if (contentType && contentType.includes("application/json")) {
-          const userInfo = response.data[0];
-          console.log(userInfo);
-          setUserData(userInfo);
-        } else {
-          console.warn("La respuesta no es de tipo JSON");
-          console.log("Contenido de la respuesta:", response.data);
-        }
-      } catch (error) {
-        console.error("Error al obtener datos del usuario:", error.message);
-      }
-    };
-    
-    
-  
-    fetchData();
-  }, [id]);
-  
-
-
+  const userData = location.state && location.state.userData;
+  console.log("userData es: ", userData);
 
   if (!userData) {
     return <div>Cargando...</div>;
@@ -49,7 +24,7 @@ const UserProfile = ({ id }) => {
         />
         <div className="user-info">
           <h1 className="profile-name">{`${userData.name} ${userData.lastname}`}</h1>
-          <h2 className='profile-id'>{`ID : ${userData.id}`}</h2>
+          <h2 className="profile-id">{`ID : ${userData.id}`}</h2>
         </div>
       </div>
       <div className="profile-details">
@@ -58,20 +33,20 @@ const UserProfile = ({ id }) => {
       </div>
 
       <div className="flipated-button">
-      <button
-        onClick={() => {
-          // Redireccionar solo si hay datos de usuario
-          if (userData) {
-            // Redirige a la galería virtual
-            window.location.href = "/multiverse";
-          }
-        }}
-      >
-        Flipado
-      </button>
+        <button
+          onClick={() => {
+            // Redireccionar solo si hay datos de usuario
+            if (userData) {
+              // Redirige a la galería virtual
+              window.location.href = "/multiverse";
+            }
+          }}
+        >
+          Flipado
+        </button>
       </div>
     </div>
   );
 };
 
-export default UserProfile;cd
+export default UserProfile;
